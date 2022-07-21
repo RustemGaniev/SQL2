58ee5732eb6b6088e4bd020323ddf1f54edbac72
RustemGaniev <ganiev@cora.local>
2022-07-21 17:37:31 +0300

    July 21

--- 3.sql
+++ 3.sql
@@ -1,4 +1,7 @@
 select product_name from ORDERS
 select id from CUSTOMERS
-where name = "alexey" or "ALEXEY"
+
 joint CUSTOMERS on ORDERS.customer_id = CUSTOMERS.id 
+where name = "alexey" or "ALEXEY";
+
+
